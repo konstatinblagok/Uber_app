6c20ab7df5f41d0b545768346932ab35d67b77c8
Sanaullah kiani <kianisanaullah@gmail.com>
2021-05-09 10:03:08 +0500

    Added email implementation for the user activation

---
+++ app/Mail/UserActiveNotification.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserActiveNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user = null;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user) {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to($this->user->email, "{$this->user->name}")
+            ->subject('Account is active!')
+            ->view('emails.user_activation');
+    }
+}

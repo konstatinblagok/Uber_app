6f2e8d936bcbeafd39392009b0db11bac086de22
Sanaullah kiani <kianisanaullah@gmail.com>
2021-05-13 05:57:09 +0500

    Removed useless code from datatables-init

--- public/dashboard/pages/datatables.init.js
+++ public/dashboard/pages/datatables.init.js
@@ -35,42 +35,6 @@
     //init
     $.DataTable = new DataTable, $.DataTable.Constructor = DataTable
 
-    const table = $('#employees-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: `${BASE_URL}/employees`,
-        dom: "lBfrtip",
-        buttons: [
-            { extend: "print", className: "btn-success"},
-            { extend: "pdf" },
-            { extend: "excel" },
-            { extend: "csv" },
-        ],
-        columns: [
-            {data: 'action', name: 'action', orderable: false, searchable: false},
-            {data: 'name', name: 'fst_name',},
-            {data: 'id', name: 'id'},
-            {data: 'email', name: 'email'},
-            {data: 'phone', name: 'phone'},
-            {data: 'hourly_rate', name: 'hourly_rate'},
-            {data: 'is_active', name: 'is_active'},
-            {data: 'address', name: 'address'},
-        ]
-    });
-
-    const docsTable = $('#documents-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: `${BASE_URL}/employees/documents?emp_id=${$('#empId').val()}`,
-        dom: "frtip",
-        columns: [
-            {data:'action', name: 'action', orderable: false, searchable: false,},
-            {data: 'name', name: 'name',},
-            {data: 'type', name: 'type'},
-            {data: 'created_at', name: 'created_at'},
-            {data: 'is_approved', name: 'is_approved'},
-        ]
-    });
 }(window.jQuery),
 
 //initializing

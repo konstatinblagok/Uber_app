0ecd66a2eb1460db47ce2ddd10892f1b42f6cca3
Sanaullah kiani <kianisanaullah@gmail.com>
2021-05-13 05:54:44 +0500

    updated view for the food-selection

--- resources/views/layouts/dashboard/food-selection.blade.php
+++ resources/views/layouts/dashboard/food-selection.blade.php
@@ -1,10 +1,10 @@
-@extends('dashboard')
+@extends('layouts.dashboard')
 @section('dashboard-content')
 <div class="content">
 
     <div class="">
         <div class="page-header-title">
-            <h4 class="page-title">Update Profile</h4>
+            <h4 class="page-title">Select your Food for today</h4>
         </div>
     </div>
 
@@ -18,53 +18,67 @@
                         <div class="panel-body">
 
                             @if (count($errors) > 0)
-                            <div class = "alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                    <div>{{ $error }}</div>
-                                    @endforeach
+                            <div class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                <div>{{ $error }}</div>
+                                @endforeach
                             </div>
                             @endif
 
-                            <form class="form-horizontal" role="form" method="POST" action="{{ route('user.update') }}"
+                            @if (\Session::has('message'))
+                            <div class="alert alert-info"> {{ \Session::get('message') }}</div>
+                            @endif
+
+                            <form class="form-horizontal m-t-30" role="form" method="POST" action="{{route('add-food-selection')}}"
                                   enctype="multipart/form-data">
                                 @csrf
                                 <div class="form-group">
-                                    <div id="profile-container" class="row">
-                                        <image id="profileImage" class="col-md-offset-5" src="{{asset($user->picture_url)}}" />
-                                    </div>
-                                    <input id="imageUpload" type="file" name="picture"
-                                           placeholder="Photo">
+                                    <label class="col-md-3 control-label">Food</label>
+                                    <div class="col-md-9">
+                                        <select id="foods-list" name="food"
+                                                class="form-control @error('food') is-invalid @enderror"
+                                                required autofocus>
+                                            <option value="" selected>Select food</option>
+                                            @foreach($foods as $food)
+                                            <option value="{{$food->id}}"
+                                                {{isset($todays_meal) && $todays_meal->todays_food==$food->id ?'selected': '' }}>{{$food->name}}</option>
+                                            @endforeach
+                                        </select>
 
-                                    @error('picture')
+                                    </div>
+                                    @error('fst_name')
                                     <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
                                 </div>
-
                                 <div class="form-group">
-                                    <label class="col-md-2 control-label">First name</label>
-                                    <div class="col-md-10">
-                                        <input id="fst_name" placeholder="First Name" type="text"
-                                               class="form-control @error('fst_name') is-invalid @enderror"
-                                               name="fst_name" value="{{ $user->fst_name }}"
-                                               required autocomplete="fst_name" autofocus>
-
+                                    <label class="col-md-3 control-label">Pickup Date</label>
+                                    <div class="col-md-9">
+                                        <div class="m-t-5">
+                                            {{\Carbon\Carbon::parse(isset($todays_meal)?$todays_meal->pickup_time:now())->format('d/m/Y')}}</div>
                                     </div>
-                                        @error('fst_name')
-                                        <span class="invalid-feedback" role="alert">
+                                    @error('lst_name')
+                                    <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
-                                        @enderror
+                                    @enderror
                                 </div>
-
                                 <div class="form-group">
-                                    <label class="col-md-2 control-label">Last name</label>
-                                    <div class="col-md-10">
-                                        <input id="lst_name" placeholder="First Name" type="text"
-                                               class="form-control @error('lst_name') is-invalid @enderror"
-                                               name="lst_name" value="{{ $user->lst_name }}"
-                                               required autocomplete="lst_name" autofocus>
+                                    <label class="col-md-3 control-label">Pickup Time</label>
+                                    <div class="col-md-9">
+                                        <!--                                        <input id="pickupTime" data-provide="datetimepicker"-->
+                                        <!--                                               class="form-control @error('pickup_time') is-invalid @enderror"-->
+                                        <!--                                               name="pickup_time" value=""-->
+                                        <!--                                               min="{{\Carbon\Carbon::now()}}"-->
+                                        <!--                                               required autocomplete="pickup_time">-->
+                                        <div class="input-group bootstrap-timepicker timepicker">
+                                            <input id="pickupTime" name="pickup_time"
+                                                   type="text" class="form-control input-small @error('pickup_time') is-invalid @enderror"
+                                                   required autocomplete="pickup_time"
+                                                   value="{{date('H:i', strtotime(isset($todays_meal)?$todays_meal->pickup_time:now()))}}">
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-time"></i></span>
+                                        </div>
 
                                     </div>
                                     @error('lst_name')
@@ -73,35 +87,24 @@
                                         </span>
                                     @enderror
                                 </div>
-
-                                <div class="form-group">
-                                    <label class="col-md-2 control-label" for="example-email">Email</label>
-                                    <div class="col-md-10">
-                                        <p class="form-control-static">{{$user->email}}</p>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-md-2 control-label">Contact #</label>
-                                    <div class="col-md-10">
-                                        <input type="number" name="phone" class="form-control" value="{{$user->phone}}">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-md-2 control-label">Address</label>
-                                    <div class="col-md-10">
-                                        <textarea name="address" class="form-control">{{$user->address}}</textarea>
-                                    </div>
-                                </div>
-
                                 <div class="form-group">
                                     <div class="col-md-offset-3 col-md-6">
                                         <button type="submit"
-                                               class="form-control btn btn-dark waves-effect waves-light m-t-10">
+                                                class="form-control btn btn-dark waves-effect waves-light m-t-10">
                                             Update
                                         </button>
                                     </div>
                                 </div>
                             </form>
+
+                            @if(isset($todays_meal))
+                            <div class="form-group text-center m-t-30">
+                                <h3>Attach Media Files</h3>
+
+                                @include('includes.dashboard.media-selection')
+
+                            </div>
+                            @endif
                         </div> <!-- panel-body -->
                     </div> <!-- panel -->
                 </div> <!-- col -->

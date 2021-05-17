fa11d4080979954e89e9455aa381901d5da14a95
Sanaullah kiani <kianisanaullah@gmail.com>
2021-05-17 08:34:19 +0500

    extracted styles and scripts from dashboard.blade.php

---
+++ resources/views/includes/dashboard/styles.blade.php
@@ -0,0 +1,28 @@
+<!-- Dropzone css -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/dropzone.min.css"
+      integrity="sha512-jU/7UFiaW5UBGODEopEqnbIAHOI8fO6T99m7Tsmqs2gkdujByJfkCbbfPSN4Wlqlb9TGnsuC0YgUgWkRBK7B9A=="
+      crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+      integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
+      crossorigin="anonymous"/>
+<link href="{{ asset('dashboard/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+<link href="{{ asset('dashboard/css/icons.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('dashboard/css/style.css') }}" rel="stylesheet" type="text/css">
+
+<!--Plugins-->
+<link href="{{ asset('dashboard/plugins/timepicker/bootstrap-timepicker.min.css')}}" rel="stylesheet">
+<!--    <link href="{{ asset('dashboard/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css')}}" rel="stylesheet">-->
+<link href="{{ asset('dashboard/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css')}}" rel="stylesheet">
+<!--    <link href="{{ asset('dashboard/plugins/bootstrap-touchspin/css/jquery.bootstrap-touchspin.min.css')}}" rel="stylesheet"/>-->
+<!-- DataTables -->
+<link href="{{ asset('dashboard/plugins/datatables/jquery.dataTables.min.css')}}" rel="stylesheet" type="text/css"/>
+<link href="{{ asset('dashboard/plugins/datatables/buttons.bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
+<!--    <link href="{{ asset('dashboard/plugins/datatables/fixedHeader.bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>-->
+<!--    <link href="{{ asset('dashboard/plugins/datatables/responsive.bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>-->
+<link href="{{ asset('dashboard/plugins/datatables/dataTables.bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
+<link href="{{ asset('dashboard/plugins/datatables/scroller.bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
+
+<!--Morris Chart CSS -->
+<!--    <link rel="stylesheet" href="{{ asset('dashboard/plugins/morris/morris.css') }}">-->
+<!--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+<link href="{{ asset('dashboard/css/custom.css') }}" rel="stylesheet">

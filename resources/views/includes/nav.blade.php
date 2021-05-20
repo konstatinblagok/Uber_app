db72edc2524e2f151022a0e02bb20e314c240dbd
Sanaullah kiani <kianisanaullah@gmail.com>
2021-05-20 12:35:24 +0500

    Updated header link text

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -11,24 +11,31 @@
             <div class="collapse navbar-collapse" id="navbars-rs-food">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active"><a class="nav-link" href="/">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{route('show-menu')}}">Menu</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown-a">
-                            <a class="dropdown-item" href="reservation.html">Reservation</a>
-                            <a class="dropdown-item" href="stuff.html">Stuff</a>
-                            <a class="dropdown-item" href="gallery.html">Gallery</a>
-                        </div>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Our Vision</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown-a">
-                            <a class="dropdown-item" href="blog.html">blog</a>
-                            <a class="dropdown-item" href="blog-details.html">blog Single</a>
-                        </div>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Order Online</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('show-menu')}}">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Shared Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Members</a>
+                    </li>
+<!--                    <li class="nav-item dropdown">-->
+<!--                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog</a>-->
+<!--                        <div class="dropdown-menu" aria-labelledby="dropdown-a">-->
+<!--                            <a class="dropdown-item" href="blog.html">blog</a>-->
+<!--                            <a class="dropdown-item" href="blog-details.html">blog Single</a>-->
+<!--                        </div>-->
+<!--                    </li>-->
                     <!-- Authentication Links -->
                     @guest
                     <li class="nav-item dropdown">

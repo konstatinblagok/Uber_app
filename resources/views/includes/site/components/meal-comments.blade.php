8e48792a7aa69f1b910c55679336a73c957f3d9a
Sanaullah kiani <kianisanaullah@gmail.com>
2021-05-23 14:08:23 +0500

    created template for meal comments using components

---
+++ resources/views/includes/site/components/meal-comments.blade.php
@@ -0,0 +1,77 @@
+<div class="blog-comment-box">
+    <h3>Comments</h3>
+    <div class="comment-item">
+        <div class="comment-item-left">
+            <img src="/site/images/avt-img.jpg" alt="">
+        </div>
+        <div class="comment-item-right">
+            <div class="pull-left">
+                <a href="#">Rubel Ahmed</a>
+            </div>
+            <div class="pull-right">
+                <i class="fa fa-clock-o" aria-hidden="true"></i>Time : <span>11.30 am</span>
+            </div>
+            <div class="des-l">
+                <p>Morbi lacinia ultrices lorem id tincidunt. Cras id dui risus. Pellentesque consectetur id mi sed pharetra. Proin imperdiet gravida nisl, sit amet varius urna. In auctor.</p>
+            </div>
+            <a href="#" class="right-btn-re"><i class="fa fa-reply" aria-hidden="true"></i> Reply</a>
+        </div>
+    </div>
+    <div class="comment-item children">
+        <div class="comment-item-left">
+            <img src="/site/images/avt-img.jpg" alt="">
+        </div>
+        <div class="comment-item-right">
+            <div class="pull-left">
+                <a href="#">Admin</a>
+            </div>
+            <div class="pull-right">
+                <i class="fa fa-clock-o" aria-hidden="true"></i>Time : <span>1.30 pm</span>
+            </div>
+            <div class="des-l">
+                <p>Morbi lacinia ultrices lorem id tincidunt. Cras id dui risus. Pellentesque consectetur id mi sed pharetra. Proin imperdiet gravida nisl, sit amet varius urna. In auctor.</p>
+            </div>
+            <a href="#" class="right-btn-re"><i class="fa fa-reply" aria-hidden="true"></i> Reply</a>
+        </div>
+    </div>
+    <div class="comment-item">
+        <div class="comment-item-left">
+            <img src="/site/images/avt-img.jpg" alt="">
+        </div>
+        <div class="comment-item-right">
+            <div class="pull-left">
+                <a href="#">Rubel Ahmed</a>
+            </div>
+            <div class="pull-right">
+                <i class="fa fa-clock-o" aria-hidden="true"></i>Time : <span>11.30 am</span>
+            </div>
+            <div class="des-l">
+                <p>Morbi lacinia ultrices lorem id tincidunt. Cras id dui risus. Pellentesque consectetur id mi sed pharetra. Proin imperdiet gravida nisl, sit amet varius urna. In auctor.</p>
+            </div>
+            <a href="#" class="right-btn-re"><i class="fa fa-reply" aria-hidden="true"></i> Reply</a>
+        </div>
+    </div>
+</div>
+<div class="comment-respond-box">
+    <h3>Leave your comment </h3>
+    <div class="comment-respond-form">
+        <form id="commentrespondform" class="comment-form-respond row" method="post">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="form-group">
+                    <input id="name_com" class="form-control" name="name" placeholder="Name" type="text">
+                </div>
+                <div class="form-group">
+                    <input id="email_com" class="form-control" name="email" placeholder="Your Email" type="email">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="form-group">
+                    <textarea class="form-control" id="textarea_com" placeholder="Your Message" rows="2"></textarea>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 mt-2 text-center">
+                <button class="btn btn-submit">Submit comment</button>
+            </div>
+        </form>
+    </div>
+</div>
